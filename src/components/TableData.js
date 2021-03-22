b760d36e33d576baa1954f329935831f0a9b63c8
Idiljei <i.jeilani@gmail.com>
2021-03-22 15:41:07 +0000

    completed data table

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -6,26 +6,30 @@ import { toTitleCase, formatNumber } from "../helpers";
 
 const TableData = () => {
   const [page, setPage] = useState(1);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [transactionHistory, settransactionHistory] = useState([]);
   const [isLoading, setIsLoading] = useState([true]);
-  // const [amount, setAmount] =([])
-
+  // const [totalSum, setTotalSum] = ([])
+  const total = []; //array of transaction amounts
 
   const getTransaction = async () => {
     try {
       const response = await fetch(`https://resttest.bench.co/transactions/${page}.json`);
       const jsonData = await response.json();
       setIsLoading(false);
-      // setAmount(jsonData.transactions.Amount);
+      const transactions = jsonData.transactions;
 
+      //! Stuck on setting the state for the total amount of transacations
+      // const amount = jsonData.transactions;
+      // setTotalSum(amount) 
+      transactions.map(item => item.Amount).reduce((prev, next) => total.push(prev + next));
+      
       if (response.status === 200) {
-        settransactionHistory(jsonData.transactions);
+        settransactionHistory(transactions);
       } else {
         error ? setError(true) : setError(false);
       }
     } catch (err) {
-      console.log("error", error);
     }
   };
 
@@ -42,6 +46,7 @@ const TableData = () => {
   };
 
 
+
   return (
     <div>
       {isLoading && <p> Please wait.. Loading list of transactions</p>}
@@ -53,7 +58,7 @@ const TableData = () => {
             <th>Date</th>
             <th>Company</th>
             <th>Account</th>
-            <th>Amount</th>
+            <th> {formatNumber(total.reduce((a, b) => a + b, 0).toFixed(2))}</th>
           </tr>
         </thead>
 
@@ -64,7 +69,7 @@ const TableData = () => {
                 <td>{data.Date ? moment(data.Date).format("MMM Do, YYYY") : ""} </td>
                 <td>{toTitleCase(data.Company)}</td>
                 <td>{data.Ledger === '' ? 'Payment' : data.Ledger}</td>
-                  <td>{data.Amount < 0 ? formatNumber(`-$${Math.abs(data.Amount).toFixed(2)}`) : formatNumber(`$${Math.abs(data.Amount).toFixed(2)}`)}</td>
+                <td>{data.Amount < 0 ? formatNumber(`-$${Math.abs(data.Amount).toFixed(2)}`) : formatNumber(`$${Math.abs(data.Amount).toFixed(2)}`)}</td>
               </tr>
             );
           })}
