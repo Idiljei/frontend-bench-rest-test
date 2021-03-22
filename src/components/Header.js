94f6e6c4ba9c70d2e026c650a9bc61d8fc2ea202
Idiljei <i.jeilani@gmail.com>
2021-03-22 04:38:12 +0000

    Styled css for Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import'../Styling/Header.css';
+
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="Header">Bench Test</h2>
+    </div>
+  );
+};
+
+export default Header; 
