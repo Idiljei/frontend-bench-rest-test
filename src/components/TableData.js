e52552d8eeb32a4f69565a029299ab19485839bc
Idiljei <i.jeilani@gmail.com>
2021-03-22 05:38:11 +0000

    table populated with transaction data

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -5,23 +5,34 @@ import * as ReactBootstrap from "react-bootstrap";
 const TableData = () => {
   const [page, setPage] = useState(1);
   const [error, setError] = useState(null);
+  const [transactionHistory, settransactionHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
 
   const getTransaction = async () => {
     try {
       const response = await fetch(`https://resttest.bench.co/transactions/${page}.json`);
       const jsonData = await response.json();
+      setIsLoading(false);
+      if (response.status === 200) {
+        settransactionHistory(jsonData.transactions);
+      } else {
+        error ? setError(true) : setError(false);
+      }
     } catch (err) {
-      error ? setError(true) : setError(false);
+      console.log("error", error);
     }
   };
 
   useEffect(() => {
     getTransaction();
-
   }, [page]);
 
   return (
     <div>
+      {isLoading && <p> Please wait.. Loading list of transactions</p>}
+      {error && <p> Sorry, an error has occured on the website </p>}
+
       <ReactBootstrap.Table striped bordered hover size="small">
         <thead>
           <tr class="table-header">
@@ -32,12 +43,16 @@ const TableData = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {transactionHistory.map((data, i) => {
+            return (
+              <tr key={i} class="table-row">
+                <td>{data.Date}</td>
+                <td>{data.Company}</td>
+                <td>{data.Ledger}</td>
+                <td>{data.Amount}</td>
+              </tr>
+            );
+          })}
 
         </tbody>
 
